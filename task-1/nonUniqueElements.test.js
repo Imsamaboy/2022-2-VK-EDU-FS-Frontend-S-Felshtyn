f81dc16fd8b7758d36ef078d6f2e5aadbbc316b5
Imsamaboy <sfelshtyn@gmail.com>
2022-09-25 20:27:14 +0300

    1 more test :)

--- task-1/nonUniqueElements.test.js
+++ task-1/nonUniqueElements.test.js
@@ -6,4 +6,5 @@ test('returns non unique elements', () => {
   expect(nonUniqueElements([1, 2, 3, 4, 5])).toEqual([]);
   expect(nonUniqueElements([5, 5, 5, 5, 5])).toEqual([5, 5, 5, 5, 5]);
   expect(nonUniqueElements([10, 9, 10, 10, 9, 8])).toEqual([10, 9, 10, 10, 9]);
+  expect(nonUniqueElements(["s", "s", 1, 2, 2, "s", "a"])).toEqual(["s", "s", 2, 2, "s"]);
 })
